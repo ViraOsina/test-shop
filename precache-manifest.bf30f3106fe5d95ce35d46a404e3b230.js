fa502831f8672c1ad096e8dd7b20f5a158c15326
Vira Osina <osinka.v@gmail.com>
2021-10-10 13:39:24 +0300

    Updates

---
+++ precache-manifest.bf30f3106fe5d95ce35d46a404e3b230.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3787179d34417692f215017897b16336",
+    "url": "/test-shop/index.html"
+  },
+  {
+    "revision": "356d7a9c26e8f5585b04",
+    "url": "/test-shop/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "dcd2b252178da688920f",
+    "url": "/test-shop/static/js/2.62a94994.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/test-shop/static/js/2.62a94994.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a28dc110561b120d29f",
+    "url": "/test-shop/static/js/3.6c2d0681.chunk.js"
+  },
+  {
+    "revision": "356d7a9c26e8f5585b04",
+    "url": "/test-shop/static/js/main.ec2297eb.chunk.js"
+  },
+  {
+    "revision": "10d289e656a016fba7c5",
+    "url": "/test-shop/static/js/runtime-main.9b047951.js"
+  }
+]);
